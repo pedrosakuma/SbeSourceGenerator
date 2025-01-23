b13aa6b232c31df11b7aaa493c1ca021d488bbf5
Pedro Travi <pedrotravi@microsoft.com>
2025-01-23 19:07:23 -0300

    Removed comments

--- PcapSbePocConsole/Models/AggregatedOrderBookEntryComparer.cs
+++ PcapSbePocConsole/Models/AggregatedOrderBookEntryComparer.cs
@@ -1,10 +1,5 @@
 ﻿namespace PcapSbePocConsole.Models
 {
-    /// <summary>
-    /// Compares two <see cref="AggregatedOrderBookEntry"/> instances by their price.
-    /// Where the price is the key for the comparison and should be in descending order
-    /// When price is null, should be on top of the list
-    /// </summary>
     public class BidAggregatedOrderBookEntryComparer : IComparer<AggregatedOrderBookEntry>
     {
         public int Compare(AggregatedOrderBookEntry? x, AggregatedOrderBookEntry? y)
@@ -24,11 +19,6 @@
             return y!.Price!.Value.CompareTo(x!.Price!.Value);
         }
     }
-    /// <summary>
-    /// Compares two <see cref="AggregatedOrderBookEntry"/> instances by their price.
-    /// Where the price is the key for the comparison and should be in ascending order
-    /// When price is null, should be on top of the list
-    /// </summary>
     public class OfferAggregatedOrderBookEntryComparer : IComparer<AggregatedOrderBookEntry>
     {
         public int Compare(AggregatedOrderBookEntry? x, AggregatedOrderBookEntry? y)
