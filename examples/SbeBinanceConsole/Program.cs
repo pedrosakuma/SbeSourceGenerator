b3421c91966fd40a9320fe4bc8cc4e4751225284
Pedro Travi <pedrotravi@microsoft.com>
2025-10-13 15:44:34 -0300

    refactor: Consolidate WebSocket subscription logic into a reusable method

--- examples/SbeBinanceConsole/Program.cs
+++ examples/SbeBinanceConsole/Program.cs
@@ -57,99 +57,88 @@ namespace SbeBinanceConsole
             ));
             await rootCommand.Parse(args).InvokeAsync();
         }
-
+        private static async Task SubscribeAsync(string? key, string? subscription, string? instrument, Action<MessageHeader, ReadOnlySpan<byte>> action, CancellationToken token)
+        {
+            ClientWebSocket ws = new ClientWebSocket();
+            ws.Options.SetRequestHeader("X-MBX-APIKEY", key);
+            Console.WriteLine("Connecting to Binance WebSocket");
+            await ws.ConnectAsync(new Uri($"wss://stream-sbe.binance.com/ws/{instrument}@{subscription}"), token);
+            Console.WriteLine("Connected to Binance WebSocket");
+            var buffer = new Memory<byte>(new byte[8192]);
+            while (!token.IsCancellationRequested)
+            {
+                var result = await ws.ReceiveAsync(buffer, token);
+                if (result.MessageType == WebSocketMessageType.Close)
+                {
+                    await ws.CloseAsync(WebSocketCloseStatus.NormalClosure, "Closing", CancellationToken.None);
+                    Console.WriteLine("WebSocket closed");
+                    break;
+                }
+                var span = buffer.Span.Slice(0, result.Count);
+                if (!MessageHeader.TryParse(span, out var header, out var body))
+                {
+                    continue;
+                }
+                action(header, body);
+            }
+        }
         private static async Task Trade(string? key, string? instrument, CancellationToken token)
         {
-            ClientWebSocket ws = new ClientWebSocket();
-            ws.Options.SetRequestHeader("X-MBX-APIKEY", key);
-            Console.WriteLine("Connecting to Binance WebSocket");
-            await ws.ConnectAsync(new Uri("wss://stream-sbe.binance.com/ws/btcusdt@trade"), token);
-            Console.WriteLine("Connected to Binance WebSocket");
-            var buffer = new Memory<byte>(new byte[8192]);
-            while (!token.IsCancellationRequested)
-            {
-                var result = await ws.ReceiveAsync(buffer, token);
-                if (result.MessageType == WebSocketMessageType.Close)
+            var tradesDataBuffer = new TradesStreamEventData.TradesData[256];
+            await SubscribeAsync(key, "trade", instrument,
+                (header, body) =>
                 {
-                    await ws.CloseAsync(WebSocketCloseStatus.NormalClosure, "Closing", CancellationToken.None);
-                    Console.WriteLine("WebSocket closed");
-                    break;
-                }
-                var span = buffer.Span.Slice(0, result.Count);
-                if (!MessageHeader.TryParse(span, out var header, out var body))
-                {
-                    continue;
-                }
-                var tradesDataBuffer = new TradesStreamEventData.TradesData[256];
-
-                switch (header.TemplateId)
-                {
-                    case TradesStreamEventData.MESSAGE_ID:
-                        if (TradesStreamEventData.TryParse(body, out var trades, out var tradesVariableData))
-                        {
-                            decimal qtyMultiplier = (decimal)Math.Pow(10, trades.QtyExponent.Value);
-                            decimal priceMultiplier = (decimal)Math.Pow(10, trades.PriceExponent.Value);
-                            int index = 0;
-                            string symbol = null;
-                            trades.ConsumeVariableLengthSegments(tradesVariableData,
-                               trade =>
-                               {
-                                   tradesDataBuffer[index++] = trade;
-                               },
-                               s =>
-                               {
-                                   symbol = Encoding.ASCII.GetString(s.VarData[..s.Length]);
-                               });
-                            for (int i = 0; i < index; i++)
+                    switch (header.TemplateId)
+                    {
+                        case TradesStreamEventData.MESSAGE_ID:
+                            if (TradesStreamEventData.TryParse(body, out var trades, out var tradesVariableData))
                             {
-                                var t = tradesDataBuffer[i];
-                                Console.WriteLine($"s:{symbol}, id: {t.Id.Value}, q: {t.Qty.Value * priceMultiplier}, p: {t.Price.Value * priceMultiplier}");
+                                decimal qtyMultiplier = (decimal)Math.Pow(10, trades.QtyExponent.Value);
+                                decimal priceMultiplier = (decimal)Math.Pow(10, trades.PriceExponent.Value);
+                                int index = 0;
+                                string? symbol = null;
+                                trades.ConsumeVariableLengthSegments(tradesVariableData,
+                                trade =>
+                                {
+                                    tradesDataBuffer[index++] = trade;
+                                },
+                                s =>
+                                {
+                                    symbol = Encoding.ASCII.GetString(s.VarData[..s.Length]);
+                                });
+                                for (int i = 0; i < index; i++)
+                                {
+                                    var t = tradesDataBuffer[i];
+                                    Console.WriteLine($"s:{symbol}, id: {t.Id.Value}, q: {t.Qty.Value * priceMultiplier}, p: {t.Price.Value * priceMultiplier}");
+                                }
                             }
-                        }
-                        break;
-                }
-            }        }
+                            break;
+                    }
+                }, token);
+        }
         private static async Task BestBidAsk(string? key, string? instrument, CancellationToken token)
         {
-            ClientWebSocket ws = new ClientWebSocket();
-            ws.Options.SetRequestHeader("X-MBX-APIKEY", key);
-            Console.WriteLine("Connecting to Binance WebSocket");
-            await ws.ConnectAsync(new Uri("wss://stream-sbe.binance.com/ws/btcusdt@bestBidAsk"), token);
-            Console.WriteLine("Connected to Binance WebSocket");
-            var buffer = new Memory<byte>(new byte[8192]);
-            while (!token.IsCancellationRequested)
-            {
-                var result = await ws.ReceiveAsync(buffer, token);
-                if (result.MessageType == WebSocketMessageType.Close)
+            await SubscribeAsync(key, "bestBidAsk", instrument,
+                (header, body) =>
                 {
-                    await ws.CloseAsync(WebSocketCloseStatus.NormalClosure, "Closing", CancellationToken.None);
-                    Console.WriteLine("WebSocket closed");
-                    break;
-                }
-                var span = buffer.Span.Slice(0, result.Count);
-                if (!MessageHeader.TryParse(span, out var header, out var body))
-                {
-                    continue;
-                }
-
-                switch (header.TemplateId)
-                {
-                    case BestBidAskStreamEventData.MESSAGE_ID:
-                        if (BestBidAskStreamEventData.TryParse(body, out var bestBid, out var bestBidVariableData))
-                        {
-                            string symbol = "";
-                            decimal qtyMultiplier = (decimal)Math.Pow(10, bestBid.QtyExponent.Value);
-                            decimal priceMultiplier = (decimal)Math.Pow(10, bestBid.PriceExponent.Value);
-                            bestBid.ConsumeVariableLengthSegments(bestBidVariableData,
-                               s =>
-                               {
-                                   symbol = Encoding.ASCII.GetString(s.VarData[..s.Length]);
-                               });
-                            Console.WriteLine($"s: {symbol}, t: {bestBid.EventTime.Value}, id:{bestBid.BookUpdateId.Value}, bq: {bestBid.BidQty.Value * qtyMultiplier}, bp: {bestBid.BidPrice.Value * priceMultiplier}, ap: {bestBid.AskPrice.Value * priceMultiplier}, aq: {bestBid.AskQty.Value * qtyMultiplier}");
-                        }
-                        break;
-                }
-            }
+                    switch (header.TemplateId)
+                    {
+                        case BestBidAskStreamEventData.MESSAGE_ID:
+                            if (BestBidAskStreamEventData.TryParse(body, out var bestBid, out var bestBidVariableData))
+                            {
+                                string symbol = "";
+                                decimal qtyMultiplier = (decimal)Math.Pow(10, bestBid.QtyExponent.Value);
+                                decimal priceMultiplier = (decimal)Math.Pow(10, bestBid.PriceExponent.Value);
+                                bestBid.ConsumeVariableLengthSegments(bestBidVariableData,
+                                   s =>
+                                   {
+                                       symbol = Encoding.ASCII.GetString(s.VarData[..s.Length]);
+                                   });
+                                Console.WriteLine($"s: {symbol}, t: {bestBid.EventTime.Value}, id:{bestBid.BookUpdateId.Value}, bq: {bestBid.BidQty.Value * qtyMultiplier}, bp: {bestBid.BidPrice.Value * priceMultiplier}, ap: {bestBid.AskPrice.Value * priceMultiplier}, aq: {bestBid.AskQty.Value * qtyMultiplier}");
+                            }
+                            break;
+                    }
+                }, token);
         }
     }
 }
